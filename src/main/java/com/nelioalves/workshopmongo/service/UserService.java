4196c3a5e7a24389d5b49ff2cc0deed32963afe3
Matheus <matheusmoraes635@gmail.com>
2024-11-10 11:15:39 -0300

    Obtendo usuario por id

--- src/main/java/com/nelioalves/workshopmongo/service/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -23,4 +24,16 @@ public class UserService {
 	}
 	
 	
+	public User findById(String id) {
+		
+		User user =  repo.findById(id).orElse(null); //uso do orElse por conta da implementação interna do Optional	
+		
+		if(user == null) {
+			
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		
+		return user;
+		
+	}
 }
