36c68f8e3fc2274c91832d0ea8588a7d301cedfe
Matheus <matheusmoraes635@gmail.com>
2024-11-24 14:28:53 -0300

    Atualização de usuário com PUT

--- src/main/java/com/nelioalves/workshopmongo/service/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/service/UserService.java
@@ -12,46 +12,63 @@ import com.nelioalves.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	
-	public List<User> findAll(){
-		
-		
+	public List<User> findAll() {
+
 		return repo.findAll();
-		
+
 	}
-	
-	
+
 	public User findById(String id) {
-		
-		User user =  repo.findById(id).orElse(null);
-		//uso do orElse por conta da implementação interna do Optional	
-		
-		if(user == null) {
-			
+
+		User user = repo.findById(id).orElse(null);
+		// uso do orElse por conta da implementação interna do Optional
+
+		if (user == null) {
+
 			throw new ObjectNotFoundException("Objeto não encontrado.");
 		}
-		
+
 		return user;
-		
+
 	}
-	
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
+
+	}
+
+	public User update(User obj) {
+
+		User newObj = repo.findById(obj.getId()).orElse(null);
+
+		if (newObj == null) {
+
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
+
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 		
 	}
-	
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
+
 }
