636c15a4b0661a376d6b3b5470069f8ca54cf832
Matheus <matheusmoraes635@gmail.com>
2024-11-30 16:54:42 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,4 +1,6 @@
-package com.nelioalves.workshopmongo.repository;
+ package com.nelioalves.workshopmongo.repository;
+
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,5 @@ import com.nelioalves.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
